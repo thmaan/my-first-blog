b12058a439bf18c263e892ae1c3e9ec4ec3bd0f8
Thomas <chucrute_thomas@hotmail.com>
2020-04-21 17:04:07 -0300

    My Django app, fifth commit

--- blog/views.py
+++ blog/views.py
@@ -9,7 +9,8 @@ from django.contrib.auth.decorators import login_required
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
-
+    
+@login_required
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     post.views += 1
@@ -30,7 +31,7 @@ def post_detail(request, pk):
         reacted = True
     else:
         reacted = False
-         
+
     if reacted:    
         percent_likes = (likes_count / (likes_count + deslikes_count))* 100
         percent_deslikes = (deslikes_count / (likes_count + deslikes_count))* 100
